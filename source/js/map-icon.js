d1d434078b951bbe6e1729aa604efbfc608609f6
Drozdov Anatolii <shadow207@mail.ru>
2020-02-10 21:31:34 +0300

    home 10-02-20

--- source/js/map-icon.js
+++ source/js/map-icon.js
@@ -3,40 +3,24 @@ ymaps.ready(init);
   function init () {
     var myMap = new ymaps.Map("map", {
       center: [59.93863106417265,30.3230545], // Центр карты, указываем коордианты
-      zoom: 17,                    // Масштаб, тут все просто
+      zoom: 178                    // Масштаб, тут все просто
       controls: ["zoomControl"],  // Отключаем все элементы управления
       behaviors: ["drag"]
     });
 
       // Наша метка, указываем коордианты
       var myGeoObjects = [];
-      if (document.body.clientWidth < 768) {
         myGeoObjects = new ymaps.Placemark([59.93863106417265,30.3230545],{
-          balloonContentBody: 'HTML Academy: Кэт Энерджи',
-          },{
+          balloonContentBody: 'HTML Academy: Кэт Энерджи',},{
           iconLayout: 'default#image',
           // Путь до нашей картинки
-          iconImageHref: '../img/map-pin.png',
+          iconImageHref: '../img/icon-map-cat.svg',
           // Размер по ширине и высоте
-          iconImageSize: [55, 53],
+          iconImageSize: [27, 49],
           // Смещение левого верхнего угла иконки относительно
           // её «ножки» (точки привязки).
           iconImageOffset: [-30, -40]
         });
-
-          } else {
-          myGeoObjects = new ymaps.Placemark([59.9385395452995, 30.323354907409684],{
-          balloonContentBody: 'HTML Academy: Кэт Энерджи',
-          },{
-          iconLayout: 'default#image',
-          // Путь до нашей картинки
-          iconImageHref: '../img/map-pin.png',
-          // Размер по ширине и высоте
-          iconImageSize: [113, 106],
-          // Смещение левого верхнего угла иконки относительно
-          // её «ножки» (точки привязки).
-          iconImageOffset: [-87, -100]
-          });
       }
 
       var clusterer = new ymaps.Clusterer({
