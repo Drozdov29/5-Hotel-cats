ef20e7f3ed4a798e6fa780dccabe36c79c4b7c02
Drozdov Anatolii <shadow207@mail.ru>
2020-02-24 00:41:14 +0300

    home 23.02.20

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,5 +1,6 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+// Меню-бутерброд (открытие - закрытие)
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -12,6 +13,22 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+// Фон с белого на желтый
+let page = document.querySelector('.page-header');
+let themeButton = document.querySelector('.main-nav__toggle');
+
+themeButton.onclick = function () {
+  console.log('Кнопка нажата!');
+  page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
+};
+
+
+// navToggle.onclick = function() {
+//   navMain.classList.toggle('main-nav--closed');
+//   navMain.classList.toggle('main-nav--opened');
+// };
 
 // navToggle.addEventListener('click', function() {
 //   if (navMain.classList.contains('main-nav--closed')) {
